ab4e94f951906dcb3bca397f782475419e9549a2
minhluquang <minhlq2911@gmail.com>
2023-06-14 14:34:22 +0700

    update source code

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ document.querySelector('.check').addEventListener('click', function () {
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
         document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('.check').disabled = true;
         if (highScore < score) {
             highScore = score;
             document.querySelector('.highscore').textContent = highScore;
